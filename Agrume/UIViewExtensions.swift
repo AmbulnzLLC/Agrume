1bb1438cee1c6e3839309dec384191d5e15467bc
John Weeks <jweeks@ambulnz.com>
2017-09-27 10:39:49 -0400

    Swift 2.3 syntax change

--- Agrume/UIViewExtensions.swift
+++ Agrume/UIViewExtensions.swift
@@ -10,7 +10,13 @@ extension UIView {
   final func snapshot() -> UIImage {
     UIGraphicsBeginImageContextWithOptions(bounds.size, true, 0)
     drawViewHierarchyInRect(bounds, afterScreenUpdates: true)
-    let snapshot = UIGraphicsGetImageFromCurrentImageContext()
+    let snapshot: UIImage
+    if let image = UIGraphicsGetImageFromCurrentImageContext() {
+      snapshot = image
+    } else {
+      snapshot = UIImage()
+      print("ERROR: Agrume UIViewExtensions.swift UIGraphicsGetImageFromCurrentImageContext() return nil")
+    }
     UIGraphicsEndImageContext()
     return snapshot
   }
